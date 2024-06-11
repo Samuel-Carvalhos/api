ccf1be65739d9205c30de4058a0b40b3ddb9e18e
Samuel de Carvalho <carvalhos.samuel86@gmail.com>
2024-06-11 20:21:49 -0300

    iniciando as rotas

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-import express from 'expres'
+import express from 'express'
 
 const app = express()
+
+app.post('/usuarios', (req, res) => {
+
+    console.log(req)
+    res.send('OK post')
+})
 
 app.get('/usuarios', (req, res) => {
     res.send('Ok, deu bom')
 })
 
+app.listen(3000)
